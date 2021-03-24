a6163e3ee9088c3637cb7ebc5db70f2a8b69e3dc
banurekhaMohan279 <banurekha279@gmail.com>
2021-03-24 11:52:53 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className = "page-center">
+      <h1> LeetCode </h1>
+      <a href = "leetcode/1-flattenArray.html" className = "next-btn"> &rarr; </a>
+    </div>
+  );
+}
+
+export default App;

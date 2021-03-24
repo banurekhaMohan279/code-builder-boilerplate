ef6a54c449293cf565c1dffab8f1f32c90957588
banurekhaMohan279 <banurekha279@gmail.com>
2021-03-24 13:06:02 +0530

    Update routes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Route path = '/'> <App/> </Route>
+      <Route exact path = '/'> <App/> </Route>
       <Route path = '/Template'> <Template/> </Route>
     </BrowserRouter>
   </React.StrictMode>,
